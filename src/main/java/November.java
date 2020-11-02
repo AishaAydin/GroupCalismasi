5a52a5169a87d52aae8d28c5745db01074d58908
AishaAydin <ayseydin13@gmail.com>
2020-11-01 21:27:04 -0500

    blue moon

--- src/main/java/November.java
+++ src/main/java/November.java
@@ -1,7 +1,7 @@
 public class November {
     public static void main(String[] args) {
         System.out.println("Today it was cold");
-        System.out.println("Yesterday there was blew moon");
+        System.out.println("Yesterday there was blue moon");
 
     }
 }
